16cad85f4f1d38f38153c9366b6e2e798dde7d70
brsaca <brsaca@gmail.com>
2023-06-30 12:24:44 -0600

    feat: initial setup of project with connection with XCAFootboolClient

---
+++ XCAFootballStats/FetchPhase.swift
@@ -0,0 +1,8 @@
+//
+//  FetchPhase.swift
+//  XCAFootballStats
+//
+//  Created by Brenda Saavedra Cantu on 30/06/23.
+//
+
+import Foundation
---
+++ XCAFootballStats/Views/StandingsTableView.swift
@@ -0,0 +1,18 @@
+//
+//  StandingsTableView.swift
+//  XCAFootballStats
+//
+//  Created by Brenda Saavedra Cantu on 30/06/23.
+//
+
+import SwiftUI
+
+struct StandingsTableView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StandingsTableView()
+}
