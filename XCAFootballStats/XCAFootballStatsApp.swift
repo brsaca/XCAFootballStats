c09867e21f09e663ee0864a54f692fbee5ff4aeb
brsaca <brsaca@gmail.com>
2023-06-30 11:16:49 -0600

    Initial Commit

---
+++ XCAFootballStats/XCAFootballStatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  XCAFootballStatsApp.swift
+//  XCAFootballStats
+//
+//  Created by Brenda Saavedra Cantu on 30/06/23.
+//
+
+import SwiftUI
+
+@main
+struct XCAFootballStatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
